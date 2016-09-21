ec9b54bd770db4f5410e0a406692bc061e527d93
givemin5 <givemin5@hotmail.com>
2016-09-21 14:37:58 +0800

    簡單的登錄程式

---
+++ LoginTest.Web/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using LoginTest.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LoginTest.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (model.Account == "givemin5" && model.Password.Equals("123"))
+                    ViewBag.Message = "登錄成功";
+                else
+                    ViewBag.Message = "登陸失敗";
+            }
+            return View();
+
+        }
+    }
+}
---
+++ LoginTest.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LoginTest.Web.MvcApplication" Language="C#" %>
